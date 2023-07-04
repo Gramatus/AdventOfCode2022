729de71ac13b78e2ebe4d24d12dd1e15b3488bba
Torgeir S <80778551+Gramatus@users.noreply.github.com>
2023-07-04 17:49:46 +0000

    Add vue and tailwind for testing

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import SomeComponent from "./SomeComponent.vue";
+const props = defineProps<{
+  msg: string;
+}>();
+console.log(props.msg);
+</script>
+
+<template>
+  <div class="block hidden text-lime-50">
+    <h1>App</h1>
+    <router-view />
+    <SomeComponent />
+  </div>
+</template>
