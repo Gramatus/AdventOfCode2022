ccfb01977fcc9bdc89f522e5dfecf4dc8a299075
Torgeir S <80778551+Gramatus@users.noreply.github.com>
2023-07-04 17:48:59 +0000

    Add terraform to devcontainer

---
+++ test.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.46.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "eastus"
+}
